0bb858bc1ea464e41dfd621c052b1b35aaf4db34
Satyam Stellar <satyam321tomar@gmail.com>
2025-05-11 01:30:25 +0530

    Create the leftside of the image

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
     <div className=" w-full h-screen sm:px-[15%] sm:py-[5%]">
       <div className={`backdrop-blur-md boder-2 border-gray-500 roundec-2xl overflow-hidden h-[100%] grid grid-cols-1 relative] ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
 
-        <LeftSideBar />
+        <LeftSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
         <ChatContainer />
         <RightSidebar />
       </div>

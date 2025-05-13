88a2748290d78cebea0928281497f3bb9d9e01c7
Satyam Stellar <satyam321tomar@gmail.com>
2025-05-13 14:46:16 +0530

    modify homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
 
   return (
     <div className=" w-full h-screen sm:px-[15%] sm:py-[5%]">
-      <div className={`backdrop-blur-md boder-2 border-gray-500 roundec-2xl overflow-hidden h-[100%] grid grid-cols-1 relative] ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+      <div className={`backdrop-blur-md border-2 border-gray-500 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative] ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
 
         <LeftSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
         <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />

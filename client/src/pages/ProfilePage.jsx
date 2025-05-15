13111bc2dfa1ce8940bea5bfa60bc54e425ff10e
Satyam Stellar <satyam321tomar@gmail.com>
2025-05-15 22:11:50 +0530

    working on profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,6 +1,52 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import assets from "../assets/assets"
+
 const ProfilePage = () => {
+
+  const [selectedImg, setSelectedImg] = useState(null)
+  const navigate = useNavigate()
+  const [name, setName] = useState("Martin Johnson")
+  const [bio, setBio] = useState("Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.")
+
   return (
-    <div>ProfilePage</div>
+    <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-lg text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form action="" className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile details</h3>
+          <label htmlFor="avatar" className="flex items-center gap-3 cursor-pointer" >
+            <input onChange={(e) => setSelectedImg(e.target.files[0])} id="avatar" accept=".png, .jpg, .jpeg" type="file" hidden />
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} className={`size-12 object-cover overflow-hidden ${selectedImg && 'rounded-full'}`} alt="" />
+            upload porfile image
+          </label>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            placeholder="Full Name"
+            required
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            rows={4}
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            placeholder="Bio"
+            required
+          />
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              navigate('/')
+            }}
+            className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-md text-lg cursor-pointer" >
+            Save
+          </button>
+        </form>
+        <img src={assets.logo_icon} className="max-w" alt="" />
+      </div>
+    </div>
   )
 }
 

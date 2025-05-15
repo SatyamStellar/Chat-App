4922cf2a79703a9f58a07018a477d27ac8c18ff6
Satyam Stellar <satyam321tomar@gmail.com>
2025-05-15 22:17:17 +0530

    complete the profile page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -7,12 +7,19 @@ const ProfilePage = () => {
   const [selectedImg, setSelectedImg] = useState(null)
   const navigate = useNavigate()
   const [name, setName] = useState("Martin Johnson")
-  const [bio, setBio] = useState("Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.")
+  const [bio, setBio] = useState("Hi everyone")
+
+  const hangleSubmit = async (e) => {
+    e.preventDefault()
+
+    navigate('/')
+
+  }
 
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
       <div className="w-5/6 max-w-2xl backdrop-blur-lg text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
-        <form action="" className="flex flex-col gap-5 p-10 flex-1">
+        <form onSubmit={hangleSubmit} action="" className="flex flex-col gap-5 p-10 flex-1">
           <h3 className="text-lg">Profile details</h3>
           <label htmlFor="avatar" className="flex items-center gap-3 cursor-pointer" >
             <input onChange={(e) => setSelectedImg(e.target.files[0])} id="avatar" accept=".png, .jpg, .jpeg" type="file" hidden />
@@ -44,7 +51,7 @@ const ProfilePage = () => {
             Save
           </button>
         </form>
-        <img src={assets.logo_icon} className="max-w" alt="" />
+        <img src={assets.logo} className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10" alt="" />
       </div>
     </div>
   )
